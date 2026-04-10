5ef96097f3abac59ff29019ebf3be05fe192b0a9
UHD NETWORK <uhdnetwork@gmail.com>
2026-04-10 20:43:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID", "1234567"))
-    API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
-    ADMIN_ID = int(os.environ.get("ADMIN_ID", "123456789"))
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-100XXXXXXXXX")
-    MONGO_URI = os.environ.get("MONGO_URI", "your_mongodb_url_here")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "0"))
+    API_HASH = os.environ.get("API_HASH", "")
+    ADMIN_ID = int(os.environ.get("ADMIN_ID", ""))
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+    
+    FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "-1003149876834") 
+    FORCE_SUB_LINK = os.environ.get("FORCE_SUB_LINK", "https://t.me/UHDBots")
+    
+    MONGO_URI = os.environ.get("MONGO_URI", "")
     DOWNLOAD_DIR = "./downloads"
     
     # Ensure download directory exists
