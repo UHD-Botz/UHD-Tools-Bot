2f9b394e1c21bcf02516ead5e66fa4ff88f12e9d
UHD NETWORK <uhdnetwork@gmail.com>
2026-04-11 16:43:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app = Client(
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
-    plugins=dict(root="handlers") # Automatically loads all files in handlers folder!
+    plugins=dict(root="handlers")
+    sleep_threshold=120
 )
 
 Config.BOT_START_TIME = time.time()
